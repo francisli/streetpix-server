e7113d6e1bda751953d802136b29b5ed141bfab9
Francis Li <mail@francisli.com>
2023-04-02 14:49:50 -0700

    [Closes #74] Updated Home page grid

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import Masonry from 'react-masonry-css';
 import { Link } from 'react-router-dom';
 
 import Api from './Api';
@@ -15,23 +14,22 @@ function Home() {
 
   return (
     <main className="container">
-      <Masonry
-        breakpointCols={{
-          default: 4,
-          1100: 3,
-          700: 2,
-          500: 1,
-        }}
-        className="home-grid"
-        columnClassName="home-grid__column">
+      <h1 className="my-4">SF Bay Street Photography</h1>
+      <div className="row">
         {photos?.map((p) => (
-          <div key={p.id} className="home-grid__item">
-            <Link to={`/members/${p.User.username}/${p.Feature.year}/${p.id}`}>
-              <img src={p.thumbUrl} alt={p.caption} className="img-fluid" />
-            </Link>
+          <div key={p.id} className="thumbnail col-md-6 col-lg-4 col-xl-3">
+            <div className="thumbnail__content">
+              <Link to={`/members/${p.User.username}/${p.Feature.year}/${p.id}`} className="square mb-3">
+                <div className="square__content" style={{ backgroundImage: `url(${p.thumbUrl})` }}></div>
+              </Link>
+              {p.caption && <div className="thumbnail__metadata fw-bolder">{p.caption}</div>}
+              <div className="thumbnail__metadata">
+                {p.User.firstName} {p.User.lastName}
+              </div>
+            </div>
           </div>
         ))}
-      </Masonry>
+      </div>
     </main>
   );
 }
