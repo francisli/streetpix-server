2103008b3c2dc21cbae29442807d844fbaa3130c
Francis Li <francisli@users.noreply.github.com>
2025-09-28 10:59:10 -0700

    [Closes #94] Ensures Meeting photos by user are sorted (#95)

--- client/src/Meetings/Meeting.jsx
+++ client/src/Meetings/Meeting.jsx
@@ -65,6 +65,7 @@ function Meeting() {
       photosByUser[user.id].push(meetingSubmission);
       photoCount += 1;
     }
+    photosByUser[user.id]?.sort((ms1, ms2) => ms1.position - ms2.position);
     if (sort === 'alpha') {
       users.sort((u1, u2) => {
         let result = u1.firstName.localeCompare(u2.firstName);
